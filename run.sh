a4ed37cc9d645e05a80a8e9a5affaf63aba49182
Daniel P. Brice <danielbrice@gmail.com>
2020-11-22 12:31:51 -0800

    Update run.sh
    People don't necessarily have `friendly` on their path (even if they should).

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@ createdb persist_ser || true
 
 ( cd preupgrade \
   && stack build \
-  && (stack run | friendly) \
+  && stack run \
   && psql -d persist_ser -c 'select * from persist_ser;' -x \
 )
 
 ( cd postupgrade \
   && stack build \
-  && (stack run | friendly) \
+  && stack run \
 )
